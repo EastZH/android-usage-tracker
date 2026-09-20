3e63a324d846fb858017ffc231b6a5ac974de7bb
EastZH <68941574+EastZH@users.noreply.github.com>
2026-09-20 12:32:17 +0800

    v1.0.1: versionCode 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         // 定在 29 可以省掉一堆 Build.VERSION 分支
         minSdk = 29
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
     }
 
     signingConfigs {
